48fb53c3b12bd1729973a0f126846e3ccab17bbb
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-06-28 11:21:45 +0100

    Fixed syntax error.

--- src/OhhhAlex/AllAPILoader/Main.php
+++ src/OhhhAlex/AllAPILoader/Main.php
@@ -17,7 +17,7 @@ class Main extends PluginBase {
         $this->getServer()->getPluginManager()->registerInterface(new AllScriptPluginLoader());
       
         if ($this->getServer()->getPluginManager()->getPlugin("DevTools") instanceof Plugin or $this->getServer()->getPluginManager()->getPlugin("FolderPluginLoader") instanceof Plugin)
-            $this->getServer()->getPluginManager()->registerInterface(new AllFolderPluginLoader($this->getServer()->getLoader());
+            $this->getServer()->getPluginManager()->registerInterface(new AllFolderPluginLoader($this->getServer()->getLoader()));
       
         $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getPluginPath(), [AllPharPluginLoader::class, AllScriptPluginLoader::class, AllFolderPluginLoader::class]);
         $this->getServer()->enablePlugins(PluginLoadOrder::STARTUP);
